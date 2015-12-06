7114d992b20ccdc5f5351180a01cd043292ee6da
Josh <jrsmith9822@gmail.com>
2015-12-06 04:12:24 +0000

    Day 2 complete

--- 2015/2/main.hs
+++ 2015/2/main.hs
@@ -3,18 +3,23 @@ import Data.List (sort)
 
 main = do
 	input <- getContents
-	
-	print (sum (map (\line -> (getSurfaceArea line) + getSmallestSide line) (lines input)))	
+	-- part 1 -- print (sum (map (\line -> (getSurfaceArea line) + getSmallestSide line) (lines input)))	
+	-- part 2
+	print (sum (map (\line -> getRibbonLength line) (lines input)))
 
 getSurfaceArea line = 2*l*w + 2*w*h + 2*h*l
 	where 
 	w = getWidth line
 	l = getLength line
 	h = getHeight line
+
 getSmallestSide line = getArea (sort (getDimensions line))
 	where 
 	getArea sortedList = (head sortedList) * (head (tail sortedList))
 
+getRibbonLength line = process (sort (getDimensions line))
+	where 
+	process sortedList = (2* head sortedList) + (2*head (tail sortedList)) + (product sortedList)
 getDimensions line = (getWidth line : ((getHeight line) : ((getLength line) : [])))
 
 getLength rawData =  read (innerProcessing rawData) :: Int 
