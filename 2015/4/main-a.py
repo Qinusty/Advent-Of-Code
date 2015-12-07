fa88d66710ba1de54195f8f5368626a67e12f762
Josh <jrsmith9822@gmail.com>
2015-12-07 12:54:38 +0000

    day 4 part a

---
+++ 2015/4/main-a.py
@@ -0,0 +1,18 @@
+import sys
+import hashlib
+
+def getHexDigest(data):
+	m = hashlib.md5()
+	m.update(data)
+	return m.hexdigest()
+
+if __name__ == "__main__":
+	data = "ckczppom"
+	currentDigest = ""
+	counter = 0
+	while not currentDigest.startswith("00000"):
+		currentDigest = getHexDigest(data + str(counter))
+		print str(counter) + " : " + currentDigest
+		counter += 1
+	print "Finished!"
+
